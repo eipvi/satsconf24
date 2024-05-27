cccc91266ea957911a0374f94f6aaf048fa603a1
Paula <eipvi@proton.me>
2024-05-27 13:36:00 -0300

    🔍️ metadata management

--- src/app/programacao/layout.jsx
+++ src/app/programacao/layout.jsx
@@ -1,13 +1,21 @@
 export const metadata = {
-    title: "Programacao",
-    description: "Programação do Satsconf 2024",
-  };
-  
+  title: "Programação",
+  description: "Programação do Satsconf 2024",
+  keywords:['programação evento satsconf', 'quais é a programação do eventos satsconf', 'programação evento 2024 bitcoin sao paulo', 'programacao satsconf 2024'],
+  openGraph:{
+    title: "Programação - O Maior Evento 100% Bitcoin do Brasil",
+    description: "Conheça os palestrantes da edição 2024",
+    type: "website",
+    url: "https://www.satsconf.com.br/programacao",
+    images: ['https://pbs.twimg.com/profile_banners/1521946346894151682/1710450716/600x200'],
+  }
+};
+
 
 export default function ProgramacaoLayout({ children }) {
-    return (
-      <>
-      {children}
-      </>
-    )
-  }
+  return (
+    <>
+    {children}
+    </>
+  )
+}
