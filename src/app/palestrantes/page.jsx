ad0c9588bfb26a4d6fc7221acf16c86800dc63a2
Paula <eipvi@proton.me>
2024-06-17 15:28:06 -0300

    ✨ adjustment to the page and speakers component

--- src/app/palestrantes/page.jsx
+++ src/app/palestrantes/page.jsx
@@ -1,13 +1,51 @@
+"use client";
+import React from "react";
+//importação global do modalNovidades
+import { useContext } from "react";
+import { ModalContext } from "@/providers/ModalContext";
 
-import BoxPrincipalPalestrantes from "@/components/(landingpage)/pages/boxpalestrantes/boxprincipalpalestrantes";
+import BtnIngresso from "@/components/(landingpage)/ui/button/BtnIngresso";
+import BtnNovidades from "@/components/(landingpage)/ui/button/BtnNovidades";
+import Card from "@/components/(landingpage)/ui/modal/modalDestaque/profilecard";
 
 const Palestrantes = ({ children }) => {
+  const { handleModalVisible } = useContext(ModalContext);
+
+  const handleOpenModal = () => {
+    handleModalVisible();
+  };
+
   return (
-    <div id="Palestrantes">
+    <section>
       {" "}
       {children}
-      <BoxPrincipalPalestrantes/>
-    </div>
+      <div id="PagPalestrantes">
+        <div className="bgContainerImageConvidadosPage">
+          <div className="bgContainerConvidadosDestaque ">
+            <div className="boxprincipalConvidadoDestaque font-white krona-one-regular ">
+              <div className="sc-mgem-16 ">
+                <span className="sc-fz-24 ">8 e 9 de Novembro | são Paulo</span>
+                <h2 className="sc-fz-48 ">
+                  CONHEÇA OS PALESTRANTES DA EDIÇÃO 2024
+                </h2>
+              </div>
+              <div className="sc-gap-8 container-btn">
+                <BtnIngresso url="https://satsconf.vercel.app/ingressos">
+                  COMPRAR INGRESSO
+                </BtnIngresso>
+                {/* botão do container principal */}
+                <BtnNovidades onClick={handleOpenModal}>
+                  RECEBER NOVIDADES
+                </BtnNovidades>
+              </div>
+            </div>
+            <div>
+              <Card />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
