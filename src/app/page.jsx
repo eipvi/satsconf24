8f11b650a40f625d65299830e8708c88bfe03fd8
Paula <eipvi@proton.me>
2024-06-25 18:42:17 -0300

    feat: hide component first

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,6 @@ import BoxBeneficios from "@/components/(landingpage)/containers/boxbeneficios";
 import BoxConvidados from "@/components/(landingpage)/containers/boxconvidados";
 // import BoxCronograma from "@/components/(landingpage)/containers/boxcronograma";
 import BoxGarante from "@/components/(landingpage)/containers/boxgarante";
-import BoxParceiros from "@/components/(landingpage)/containers/boxparceiros";
-import BoxPatrocinadores from "@/components/(landingpage)/containers/boxpatrocinadores";
 import BoxPrincipal from "@/components/(landingpage)/containers/boxprincipal";
 import BoxSobre from "@/components/(landingpage)/containers/boxsobre";
 
