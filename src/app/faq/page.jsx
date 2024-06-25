17ab0abb63460d11976276306bca019a11ea08b0
Paula <eipvi@proton.me>
2024-06-25 18:28:31 -0300

    feat: insert component on faq page

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,36 +1,46 @@
+import FAQComponent from "@/components/(landingpage)/containers/faq";
 import FAQpreparese from "@/components/(landingpage)/containers/faq/faqpreparese";
 import FAQqueromeenvolver from "@/components/(landingpage)/containers/faq/faqqueromeenvolver";
 import ImageFaq from "@/components/(landingpage)/images/ImageFaq";
 
-
 const Faq = ({ children }) => {
   return (
     <div id="Faq">
-            {/* Aqui é onde você renderiza children */} {children}
+      {/* Aqui é onde você renderiza children */} {children}
       <div className="bgContainerImgFaq ">
-      <div className="bgContainerPrincipal ">
-        <div className=" bgContainerMargin">
-          <div className="boxprincipal">
-            {/* caixa co container com fonte do titulo */}
-            <div className="sc-bg-container krona-one-regular">
-              <h1>TIRE SUAS DÚVIDAS SOBRE A SATSCONF</h1>
-              {/* gap de 16 com container de botão */}
-            </div>
+        <div className="bgContainerPrincipal ">
+          <div className=" bgContainerMargin">
+            <div className="boxprincipal">
+              {/* caixa co container com fonte do titulo */}
+              <div className="sc-bg-container krona-one-regular">
+                <h1>TIRE SUAS DÚVIDAS SOBRE A SATSCONF</h1>
+                {/* gap de 16 com container de botão */}
+              </div>
 
-            <div className="btningresso-img container-com-gradiente">
-              {/* imagem do container principal da pagina de ingresso esportado como componente */}
-            <ImageFaq />
-            </div>
-
+              <div className="btningresso-img container-com-gradiente">
+                {/* imagem do container principal da pagina de ingresso esportado como componente */}
+                <ImageFaq />
+              </div>
             </div>
           </div>
         </div>
       </div>
       <div className=" krona-one-regular bgContaineImgIgresso">
-      <FAQqueromeenvolver />
+        <FAQqueromeenvolver />
+      </div>
+      <div className="krona-one-regular bgContaineImgIgresso">
+        <FAQComponent />
       </div>
       <FAQpreparese />
+      <div className="boxTitle padding-bottom-30">
+        <h2 className="krona-one-regular ">Não encontrou a resposta para sua pergunta?</h2>
+        <p className="">
+          Entre em contato por e-mail (info@satsconf.com.br), pelo formulário do
+          site, ou visite nossos grupos no Telegram ou Whatsapp, <br /> onde alguém da
+          organização ou mesmo outro participante do evento podem te ajudar.
+        </p>
       </div>
+    </div>
   );
 };
 
